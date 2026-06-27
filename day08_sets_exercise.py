70b5ede646afc0bf595afcee8779f0018a293679
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-06-27 19:28:32 +0500

    Update day08_sets_exercise.py

--- day08_sets_exercise.py
+++ day08_sets_exercise.py
@@ -6,15 +6,24 @@
 # name = input('Enter your name: ')
 # age = input('Enter your age: ')
 # greeting(name, 32)
+# 3. Capture the color via an input box as variable:color 
+color= str(input("Enter your favourite color: "))
 # 1. Add new print statement - on a new line
 #    which says 'We hear you like the color xxx! xxx is a string with color 
-color= str(input("Enter your favourite color: "))
-def print_color(color):
-    print(f'We hear you like the color: {color}!')
+# 6. Favorite color should be in lowercase 
+def print_color(color='red'):
+    print(f'We hear you like the color: {color.lower()}!')
 print_color(color)
 # 2. extend the function with another  input parameter 'color', that defaults to 'red'
-# 3. Capture the color via an input box as variable:color 
+print_color()
+
+def greeting(name,age=28):
+    print(f'Hello {name.capitalize()}, you are {age} years old.')
+def birthday(name,age=28):
+    print(f'Hello {name.capitalize()}, you will be {age + 1} next birthday.')
+
+greeting("Fakhar",28)
+birthday("Fakhar",28)
 # 4. Change the 'You are xx!' text to say 'you will be xx+1 years old next birthday 
 #  adding 1 to the age
 # 5. Capitalize first letter of the 'name', and rest are small caps 
-# 6. Favorite color should be in lowercase 
