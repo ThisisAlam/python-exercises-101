3c7f04785de46bf30dd90d4e4e75582fc9feb209
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-06-30 13:49:59 +0500

    updated the codebase

--- day20_file_handling_part2.py
+++ day20_file_handling_part2.py
@@ -18,11 +18,32 @@
 # print(file.closed)
 # print(file.readline())
 
-with open("notes.txt","w") as file:
-    result= file.write("python")
-    print(result)
-print(result)
+# with open("notes.txt","w") as file:
+#     result= file.write("python")
+#     print(result)
+# print(result)
 
-with open("justice.txt","w") as file:
-    file.write("Batman")
-    file.write("Superman")
+# with open("justice.txt","w") as file:
+#     file.write("Batman")
+#     file.write("Superman")
+
+
+cart = {
+    "iron sword": {
+        "price": 120,
+        "description": "A reliable sword."
+    },
+    "bread": {
+        "price": 5,
+        "description": "Freshly baked."
+    }
+}
+with open("cart.txt", "a") as file:
+    file.write(f"{'=' * 30}\n")
+    file.write("SHOPPING CART\n")
+    file.write(f"{'=' * 30}\n")
+    for key, value in cart.items():
+        file.write(f"Item: {key}\n")
+        file.write(f"Price: {value['price']}\n")
+        file.write(f"Description: {value['description']}\n")
+        file.write("-" * 30 + "\n\n")
